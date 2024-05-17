210842070c38e330cfbbe845331d518865d4b014
Mike Blake <mike@chikoon.com>
2024-05-17 13:08:22 +0200

    Deploy script   - clean up   - include merge of develop branch into main branch Moved tarifas.html to old/

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,22 +8,19 @@ APPHOME='~/code/tuapegoseguro'
 echo "# Begin TuApegoSeguro deployment"
 echo
 
-echo "# Push ${APPNAME}.main to github"
+echo "# Merge ${APPNAME}.local develop to main"
 echo "> git co main"
 git checkout main
+echo "> git merge develop"
+git merge develop
+
+echo "# Push ${APPNAME}.main to github"
 echo "> git push origin main"
 git push origin main
 
 echo "# Push ${APPNAME}.main to production"
 echo "> git push -f ${APPUSER}@${APPHOST}:~/code/${APPNAME} +main:new-main"
 git push -f ${APPUSER}@${APPHOST}:~/code/${APPNAME} +main:new-main
-
-# echo "> git push ${APPUSER}@${APPHOST}:~/code/${APPNAME} +main:new-main"
-# git push ${APPUSER}@${APPHOST}:~/${APPNAME} +main:new-main
-
-# echo "> git push ${APPUSER}@${APPHOST}:~/${APPNAME}.pro.git +main:new-main"
-# git push ${APPUSER}@${APPHOST}:~/${APPNAME}.pro.git +main:new-main
-# echo
 
 echo "# Done! Now, go to production as ${APPUSER} and unpack it -------------------"
 echo "> cd ${APPHOME}; git co main; git merge new-main"
